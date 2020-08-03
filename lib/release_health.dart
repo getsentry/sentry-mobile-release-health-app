ea4acdb27893fe25340213ffdae1973900e6eede
Matej Minar <matej.minar@sentry.io>
2020-08-03 20:42:16 +0200

    feat: Add more Release class properties

--- lib/release_health.dart
+++ lib/release_health.dart
@@ -20,17 +20,54 @@ Future<List<Release>> fetchReleases() async {
 }
 
 class Release {
-  Release({this.version, this.crashFreeSessions});
+  Release(
+      {this.version,
+      this.project,
+      this.date,
+      this.issues,
+      this.crashes,
+      this.crashFreeUsers,
+      this.crashFreeSessions,
+      this.users24h,
+      this.usersTotal,
+      this.sessions24h,
+      this.sessionsTotal,
+      this.durationP90});
 
   factory Release.fromJson(dynamic json) {
     return Release(
-        version: json['version'] as String,
+        version: json['versionInfo']['description'] as String,
+        project: json['projects'][0]['slug'] as String,
+        date: json['dateCreated']
+            as String, // lastDeploy.dateFinished ?? dateCreated
+        issues: json['newGroups'] as int,
+        crashes: json['projects'][0]['healthData']['sessionsCrashed'] as int,
+        crashFreeUsers:
+            json['projects'][0]['healthData']['crashFreeUsers'] as double,
         crashFreeSessions:
-            json['projects'][0]['healthData']['crashFreeSessions'] as double);
+            json['projects'][0]['healthData']['crashFreeSessions'] as double,
+        users24h: json['projects'][0]['healthData']['totalUsers24h'] as int,
+        usersTotal: json['projects'][0]['healthData']['totalUsers'] as int,
+        sessions24h:
+            json['projects'][0]['healthData']['totalSessions24h'] as int,
+        sessionsTotal:
+            json['projects'][0]['healthData']['totalSessions'] as int,
+        durationP90:
+            json['projects'][0]['healthData']['durationP90'] as double);
   }
 
   final String version;
+  final String project;
+  final String date;
+  final int issues;
+  final int crashes;
+  final double crashFreeUsers;
   final double crashFreeSessions;
+  final int users24h;
+  final int usersTotal;
+  final int sessions24h;
+  final int sessionsTotal;
+  final double durationP90;
 }
 
 class ReleaseHealth extends StatefulWidget {
