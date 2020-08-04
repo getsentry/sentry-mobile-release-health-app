af62c71338cc8c9cc370b095fbd3058acb9aa448
Jennarong Muengtaweepongsa <ldrwarlock@gmail.com>
2020-08-04 15:35:32 +0700

    Working issue sample

--- lib/types/group.dart
+++ lib/types/group.dart
@@ -3,15 +3,15 @@ class Group {
   final String id;
   final String culprit;
   final String lastSeen;
-  final String userCount;
-  final String count;
+  final int userCount;
+  final int count;
   final String title;
 
   Group.fromJson(Map<String, dynamic> json)
       : id = json['id'] as String,
         culprit = json['culprit'] as String,
         lastSeen = json['lastSeen'] as String,
-        userCount = json['userCount'] as String,
-        count = json['count'] as String,
+        userCount = json['userCount'] as int,
+        count = json['count'] as int,
         title = json['title'] as String;
 }
