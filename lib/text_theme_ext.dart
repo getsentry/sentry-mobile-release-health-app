0a87fd5963ff80658a5b91dab59e24bb77c98a92
Jenn Mueng <me@jennmueng.com>
2020-08-04 19:53:02 +0700

    deps: Google fonts dependency and custom code textTheme

---
+++ lib/text_theme_ext.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+extension CustomStyles on TextTheme {
+  TextStyle get code => GoogleFonts.robotoMono();
+}
