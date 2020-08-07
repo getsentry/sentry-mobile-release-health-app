b7d098ed51848a68ba5c825b0037d16e46dea054
Jenn Mueng <me@jennmueng.com>
2020-08-07 16:28:57 +0700

    feat: Real data for issues screen

--- lib/issues.dart
+++ lib/issues.dart
@@ -1,95 +1,38 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:http/http.dart' as http;
+import 'package:redux/redux.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
-import './types/group.dart';
 import 'issue_screen.dart';
+import 'redux/state/app_state.dart';
+import 'types/group.dart';
+import 'types/organization.dart';
+import 'types/project.dart';
 
-const String jsonString = '''
-  {
-    "groups": [
-      {
-        "platform": "cocoa",
-        "lastSeen": "2020-08-02T16:39:34.994565Z",
-        "numComments": 0,
-        "userCount": 5,
-        "stats": {},
-        "culprit": "?(app:///main.jsbundle)",
-        "title": "Error: Failed to grant permission",
-        "id": "1773677544",
-        "assignedTo": null,
-        "logger": "javascript",
-        "type": "error",
-        "annotations": [],
-        "metadata": {
-            "function": "value",
-            "type": "Error",
-            "value": "Failed to grant permission",
-            "filename": "app:///main.jsbundle"
-        },
-        "status": "unresolved",
-        "subscriptionDetails": null,
-        "isPublic": false,
-        "hasSeen": false,
-        "shortId": "TOUR-MOBILE-3ED",
-        "shareId": null,
-        "firstSeen": "2020-07-08T20:10:22.111494Z",
-        "count": "5",
-        "permalink": "https://sentry.io/organizations/monos-digital/issues/1773677544/",
-        "level": "error",
-        "isSubscribed": false,
-        "isBookmarked": false,
-        "project": {
-            "platform": "react-native",
-            "slug": "tour-mobile",
-            "id": "1459798",
-            "name": "React-Native"
-        },
-        "statusDetails": {}
-      },
-      {
-        "platform": "javascript",
-        "lastSeen": "2020-08-02T12:23:25.090695Z",
-        "numComments": 0,
-        "userCount": 6,
-        "stats": {},
-        "culprit": "performSyncWorkOnRoot([native code])",
-        "title": "Error: Text strings must be rendered within a <Text> component.",
-        "id": "1793417200",
-        "assignedTo": null,
-        "logger": null,
-        "type": "error",
-        "annotations": [],
-        "metadata": {
-            "function": "performSyncWorkOnRoot",
-            "type": "Error",
-            "value": "Text strings must be rendered within a <Text> component.",
-            "filename": "[native code]"
-        },
-        "status": "unresolved",
-        "subscriptionDetails": null,
-        "isPublic": false,
-        "hasSeen": true,
-        "shortId": "TOUR-MOBILE-3VH",
-        "shareId": null,
-        "firstSeen": "2020-07-19T08:18:00.302863Z",
-        "count": "23",
-        "permalink": "https://sentry.io/organizations/monos-digital/issues/1793417200/",
-        "level": "error",
-        "isSubscribed": false,
-        "isBookmarked": false,
-        "project": {
-            "platform": "react-native",
-            "slug": "tour-mobile",
-            "id": "1459798",
-            "name": "React-Native"
-        },
-        "statusDetails": {}
-      }
-    ]
+Future<List<Group>> fetchGroups(
+    String orgSlug, String projSlug, Cookie cookie) async {
+  final response = await http.get(
+      'https://sentry.io/api/0/projects/$orgSlug/$projSlug/issues/',
+      headers: {HttpHeaders.cookieHeader: cookie.toString()});
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    final List<dynamic> responseJson =
+        json.decode(response.body) as List<dynamic>;
+    return responseJson
+        .map((dynamic json) => Group.fromJson(json as Map<String, dynamic>))
+        .toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Groups');
   }
-''';
+}
 
 class IssuesScreenBuilder extends StatelessWidget {
   const IssuesScreenBuilder({Key key}) : super(key: key);
@@ -118,79 +61,92 @@ class IssuesScreenBuilder extends StatelessWidget {
   }
 }
 
-class IssuesScreen extends StatefulWidget {
-  const IssuesScreen({Key key}) : super(key: key);
-
+class IssuesScreen extends StatelessWidget {
   @override
-  _IssuesScreenState createState() => _IssuesScreenState();
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, IssuesViewModel>(
+      converter: (store) => IssuesViewModel.fromStore(store),
+      builder: (_, viewModel) => IssuesScreenStateWrapper(viewModel: viewModel),
+    );
+  }
 }
 
-class _IssuesScreenState extends State<IssuesScreen> {
-  List<Group> sampleGroups = [];
+class IssuesViewModel {
+  IssuesViewModel(
+      {@required this.selectedOrganization,
+      @required this.selectedProject,
+      @required this.sessionCookie});
+
+  final Organization selectedOrganization;
+  final Project selectedProject;
+  final Cookie sessionCookie;
+
+  static IssuesViewModel fromStore(Store<AppState> store) => IssuesViewModel(
+      selectedOrganization: store.state.globalState.selectedOrganization,
+      selectedProject: store.state.globalState.selectedProject,
+      sessionCookie: store.state.globalState.session);
+}
+
+class IssuesScreenStateWrapper extends StatefulWidget {
+  const IssuesScreenStateWrapper({Key key, @required this.viewModel})
+      : super(key: key);
+
+  final IssuesViewModel viewModel;
+
+  @override
+  _IssuesScreenState createState() => _IssuesScreenState(viewModel: viewModel);
+}
+
+class _IssuesScreenState extends State<IssuesScreenStateWrapper> {
+  _IssuesScreenState({@required this.viewModel});
+
+  final IssuesViewModel viewModel;
+  Future<List<Group>> groupsFuture;
 
   @override
   void initState() {
     super.initState();
-    getSampleData();
+    getData();
   }
 
-  void getSampleData() {
+  void getData() {
     setState(() {
-      Map<String, dynamic> groups =
-          jsonDecode(jsonString) as Map<String, dynamic>;
-
-      final List<dynamic> groupLists = groups['groups'] as List<dynamic>;
-      sampleGroups = groupLists
-          .map((dynamic groupJson) =>
-              Group.fromJson(groupJson as Map<String, dynamic>))
-          .toList();
+      groupsFuture = fetchGroups(viewModel.selectedOrganization.slug,
+          viewModel.selectedProject.slug, viewModel.sessionCookie);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-            height: 34,
-            color: Colors.grey[200],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                    width: 48,
-                    alignment: Alignment.centerRight,
-                    child: Text('EVENTS',
-                        style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.w500))),
-                Container(
-                    margin: EdgeInsets.only(right: 16.0),
-                    width: 48,
-                    alignment: Alignment.centerRight,
-                    child: Text('USERS',
-                        style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.w500)))
-              ],
-            )),
-        Expanded(
-          child: ListView.builder(
-            itemCount: sampleGroups.length,
-            itemBuilder: (context, index) {
-              final group = sampleGroups[index];
+    return FutureBuilder(
+        future: groupsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<Group> groups = snapshot.data as List<Group>;
 
-              return Issue(
-                  title: group.metadata.type,
-                  value: group.title,
-                  culprit: group.culprit,
-                  userCount: group.userCount,
-                  count: group.count,
-                  lastSeen: group.lastSeen,
-                  firstSeen: group.lastSeen);
-            },
-          ),
-        )
-      ],
-    );
+            return ListView.builder(
+              itemCount: groups.length,
+              itemBuilder: (context, index) {
+                final group = groups[index];
+
+                return Issue(
+                    title: group.metadata.type ?? 'Error',
+                    value: group.title,
+                    culprit: group.culprit,
+                    userCount: group.userCount,
+                    count: group.count,
+                    lastSeen: group.lastSeen,
+                    firstSeen: group.lastSeen);
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
 
