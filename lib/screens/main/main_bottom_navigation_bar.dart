1e504ba93c0ee5480a71e8acd1f5cfc9b75efa9c
denrase <denrase@gmail.com>
2020-12-22 11:24:29 +0100

    Colid line for tabbar instead of shadow

--- lib/screens/main/main_bottom_navigation_bar.dart
+++ lib/screens/main/main_bottom_navigation_bar.dart
@@ -12,38 +12,44 @@ class MainBottomNavigationBar extends StatelessWidget {
     return StoreConnector<AppState, AppState>(
         converter: (store) => store.state,
         builder: (context, state) {
-          return BottomNavigationBar(
-              currentIndex: state.globalState.selectedTab,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              backgroundColor: Colors.white,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(SentryIcons.home,
-                    color: Color(0xffB9C1D9),
-                    size: 24.0,
-                  ),
-                  activeIcon: Icon(SentryIcons.home,
-                    color: Color(0xff81B4FE),
-                    size: 24.0,
-                  ),
-                  title: Text('Health'),
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(SentryIcons.issues,
+          return Container(
+            decoration: BoxDecoration(
+                border: Border(top: BorderSide(color: Color(0x44B9C1D9)))
+            ),
+            child: BottomNavigationBar(
+                elevation: 0,
+                currentIndex: state.globalState.selectedTab,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                backgroundColor: Colors.white,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(SentryIcons.home,
                       color: Color(0xffB9C1D9),
                       size: 24.0,
                     ),
-                    activeIcon: Icon(SentryIcons.issues,
+                    activeIcon: Icon(SentryIcons.home,
                       color: Color(0xff81B4FE),
                       size: 24.0,
                     ),
-                    title: Text('Issues')
-                )
-              ],
-              onTap: (int index) {
-                StoreProvider.of<AppState>(context).dispatch(SwitchTabAction(index));
-              }
+                    title: Text('Health'),
+                  ),
+                  BottomNavigationBarItem(
+                      icon: Icon(SentryIcons.issues,
+                        color: Color(0xffB9C1D9),
+                        size: 24.0,
+                      ),
+                      activeIcon: Icon(SentryIcons.issues,
+                        color: Color(0xff81B4FE),
+                        size: 24.0,
+                      ),
+                      title: Text('Issues')
+                  )
+                ],
+                onTap: (int index) {
+                  StoreProvider.of<AppState>(context).dispatch(SwitchTabAction(index));
+                }
+            ),
           );
         }
     );
