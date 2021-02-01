3ff9bd8e65ecd96eed88bcfe0c428adaf67aec21
denrase <denrase@gmail.com>
2021-02-01 10:11:05 +0100

    Fix ReleaseHealthChartRowViewModel tests

--- test/screens/release_health/release_health_chart_row_view_model_test.dart
+++ test/screens/release_health/release_health_chart_row_view_model_test.dart
@@ -1,103 +1,128 @@
+import 'package:sentry_mobile/redux/state/session_state.dart';
 import 'package:sentry_mobile/screens/chart/line_chart_point.dart';
 import 'package:sentry_mobile/screens/release_health/release_health_chart_row_view_model.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('createByHalvingPoints', () {
-    test('data is trailing', () {
+    test('data is points', () {
+
+      final pointsBefore = [
+        LineChartPoint(0, 0),
+        LineChartPoint(1, 5),
+      ];
 
       final points = [
         LineChartPoint(0, 0),
         LineChartPoint(1, 10),
-        LineChartPoint(2, 0),
-        LineChartPoint(3, 5),
       ];
 
-      final sut = ReleaseHealthChartRowViewModel.create(points, []);
+      final sessionState = SessionState(
+          projectId: 'fixture-projectId',
+          sessionCount: 10,
+          previousSessionCount: 5,
+          points: points,
+          previousPoints: pointsBefore
+      );
+
+      final sut = ReleaseHealthChartRowViewModel.create(sessionState, []);
       expect(sut.data.points,
-        equals([LineChartPoint(0, 0), LineChartPoint(1, 5)]) // x normalized
+        equals([LineChartPoint(0, 0), LineChartPoint(1, 10)])
       );
     });
 
-    test('data is trailing uneven points', () {
-
-      final points = [
-        LineChartPoint(0, 1000000),
-        LineChartPoint(1, 0),
-        LineChartPoint(2, 10),
-        LineChartPoint(3, 0),
-        LineChartPoint(4, 5),
+    test('percentChange increase', () {
+      final pointsBefore = [
+        LineChartPoint(0, 0),
+        LineChartPoint(1, 5),
       ];
-
-      final sut = ReleaseHealthChartRowViewModel.create(points, []);
-      expect(sut.data.points,
-          equals([LineChartPoint(0, 0), LineChartPoint(1, 5)]) // x normalized
-      );
-    });
-
-    test('percentChange', () {
 
       final points = [
         LineChartPoint(0, 0),
         LineChartPoint(1, 10),
-        LineChartPoint(2, 0),
-        LineChartPoint(3, 5),
       ];
 
-      final sut = ReleaseHealthChartRowViewModel.create(points, []);
+      final sessionState = SessionState(
+          projectId: 'fixture-projectId',
+          sessionCount: 10,
+          previousSessionCount: 5,
+          points: points,
+          previousPoints: pointsBefore
+      );
+
+      final sut = ReleaseHealthChartRowViewModel.create(sessionState, []);
       expect(sut.percentChange,
-          equals(-50.0)
+          equals(100.0)
       );
     });
 
-    test('percentChange uneven points', () {
-
-      final points = [
-        LineChartPoint(0, 1000000),
-        LineChartPoint(1, 0),
-        LineChartPoint(2, 10),
-        LineChartPoint(3, 0),
-        LineChartPoint(4, 5),
+    test('percentChange decrease', () {
+      final pointsBefore = [
+        LineChartPoint(0, 0),
+        LineChartPoint(1, 10),
       ];
-
-      final sut = ReleaseHealthChartRowViewModel.create(points, []);
-      expect(sut.percentChange,
-          equals(-50.0)
-      );
-    });
-
-    test('numberOfIssues', () {
 
       final points = [
         LineChartPoint(0, 0),
-        LineChartPoint(1, 10),
-        LineChartPoint(2, 0),
-        LineChartPoint(3, 5),
+        LineChartPoint(1, 8),
       ];
 
-      final sut = ReleaseHealthChartRowViewModel.create(points, []);
-      expect(sut.numberOfIssues,
-          equals(5)
+      final sessionState = SessionState(
+          projectId: 'fixture-projectId',
+          sessionCount: 8,
+          previousSessionCount: 10,
+          points: points,
+          previousPoints: pointsBefore
+      );
+
+      final sut = ReleaseHealthChartRowViewModel.create(sessionState, []);
+      expect(sut.percentChange,
+          equals(-20.0)
       );
     });
 
     test('parentPoints yMax value', () {
+      final pointsBefore = [
+        LineChartPoint(0, 0),
+        LineChartPoint(1, 5),
+      ];
 
       final points = [
         LineChartPoint(0, 0),
         LineChartPoint(1, 10),
-        LineChartPoint(2, 0),
-        LineChartPoint(3, 5),
       ];
+
+      final sessionState = SessionState(
+          projectId: 'fixture-projectId',
+          sessionCount: 10,
+          previousSessionCount: 5,
+          points: points,
+          previousPoints: pointsBefore
+      );
 
       final parentPoints = [
         LineChartPoint(0, 0),
         LineChartPoint(1, 1000)
       ];
 
-      final sut = ReleaseHealthChartRowViewModel.create(points, parentPoints);
+      final sut = ReleaseHealthChartRowViewModel.create(sessionState, parentPoints);
       expect(sut.data.maxY,
           equals(1000)
+      );
+    });
+
+    test('fallback no points', () {
+      final sessionState = SessionState(
+          projectId: 'fixture-projectId',
+          sessionCount: 0,
+          previousSessionCount: 0,
+          points: [],
+          previousPoints: []
+      );
+
+      final sut = ReleaseHealthChartRowViewModel.create(sessionState, []);
+      expect(sut.data.points,
+          equals([LineChartPoint(0, 0), LineChartPoint(1, 0)])
       );
     });
   });
