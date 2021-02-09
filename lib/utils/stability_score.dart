d70b13810dd359cd389014d8b0cdd2c925966a2d
denrase <denrase@gmail.com>
2021-02-09 13:34:27 +0100

    DRY methods

--- lib/utils/stability_score.dart
+++ lib/utils/stability_score.dart
@@ -1,49 +1,36 @@
+import 'package:sentry_mobile/types/session_group.dart';
+
 import '../types/session_status.dart';
 import '../types/sessions.dart';
 
 extension StabilityScore on Sessions {
   double crashFreeSessions() {
-    int healthy;
-    int errored;
-    int abnormal;
-    int crashed;
-    for (final group in groups) {
-      if (group?.by?.sessionStatus == SessionStatus.healthy) {
-        healthy = group?.totals?.sumSession;
-      } else if (group?.by?.sessionStatus == SessionStatus.errored) {
-        errored = group?.totals?.sumSession;
-      } else if (group?.by?.sessionStatus == SessionStatus.abnormal) {
-        abnormal = group?.totals?.sumSession;
-      } else if (group?.by?.sessionStatus == SessionStatus.crashed) {
-        crashed = group?.totals?.sumSession;
-      }
-    }
-    if (healthy == null || errored == null || abnormal == null || crashed == null) {
-      return null;
-    }
-    final sum = healthy + abnormal + crashed + errored;
-    if (sum == 0) {
-      return null;
-    } else {
-      return 100 - (crashed / sum) * 100;
-    }
+    return _crashFree((SessionGroup group) {
+      return group?.totals?.sumSession;
+    });
   }
 
-  // TODO(denis): DRY
   double crashFreeUsers() {
+    return _crashFree((SessionGroup group) {
+      return group?.totals?.countUniqueUsers;
+    });
+  }
+
+  // Higher order function to calculate crash free value
+  double _crashFree(int Function(SessionGroup group) valueFromGroup) {
     int healthy;
     int errored;
     int abnormal;
     int crashed;
     for (final group in groups) {
       if (group?.by?.sessionStatus == SessionStatus.healthy) {
-        healthy = group?.totals?.countUniqueUsers;
+        healthy = valueFromGroup(group);
       } else if (group?.by?.sessionStatus == SessionStatus.errored) {
-        errored = group?.totals?.countUniqueUsers;
+        errored = valueFromGroup(group);
       } else if (group?.by?.sessionStatus == SessionStatus.abnormal) {
-        abnormal = group?.totals?.countUniqueUsers;
+        abnormal = valueFromGroup(group);
       } else if (group?.by?.sessionStatus == SessionStatus.crashed) {
-        crashed = group?.totals?.countUniqueUsers;
+        crashed = valueFromGroup(group);
       }
     }
     if (healthy == null || errored == null || abnormal == null || crashed == null) {
@@ -56,6 +43,4 @@ extension StabilityScore on Sessions {
       return 100 - (crashed / sum) * 100;
     }
   }
-  
-  
 }
