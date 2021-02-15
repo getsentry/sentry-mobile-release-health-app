61db80baff9d45f78f539e5ab7a85fd0368a89bd
denrase <denrase@gmail.com>
2021-02-15 16:50:34 +0100

    Remove unused import

--- lib/screens/login/login_web_view.dart
+++ lib/screens/login/login_web_view.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_user_agent/flutter_user_agent.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
