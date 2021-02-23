4f5104e3617c192179d82425716ef388861bd5d0
denrase <denrase@gmail.com>
2021-02-23 14:42:23 +0100

    Don't resize onboarding for keyboard

--- lib/screens/oboarding/onboarding_screen.dart
+++ lib/screens/oboarding/onboarding_screen.dart
@@ -44,6 +44,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
