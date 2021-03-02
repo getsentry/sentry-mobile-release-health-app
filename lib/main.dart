5affe8195d0dcdf24b69a9af1c5d92fae6c4486b
denrase <denrase@gmail.com>
2021-03-02 12:47:24 +0100

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
