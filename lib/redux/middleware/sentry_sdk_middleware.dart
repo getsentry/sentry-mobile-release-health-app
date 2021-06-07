7235a5d47570d96f71190835558326c6f1962568
Jonas Uekötter <dev@uekoetter.dev>
2021-06-07 14:38:21 +0200

    Only consider sentry_mobile as in app

--- lib/redux/middleware/sentry_sdk_middleware.dart
+++ lib/redux/middleware/sentry_sdk_middleware.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/foundation.dart';
 import 'package:redux/redux.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
@@ -8,9 +6,9 @@ import '../../redux/actions.dart';
 import '../../redux/state/app_state.dart';
 
 class SentrySdkMiddleware extends MiddlewareClass<AppState> {
-
   @override
-  dynamic call(Store<AppState> store, dynamic action, NextDispatcher next) async {
+  dynamic call(
+      Store<AppState> store, dynamic action, NextDispatcher next) async {
     if (action is RehydrateSuccessAction) {
       if (action.sentrySdkEnabled && !Sentry.isEnabled) {
         _enableSentrySdk();
@@ -29,7 +27,8 @@ class SentrySdkMiddleware extends MiddlewareClass<AppState> {
     if (action is FetchAuthenticatedUserSuccessAction) {
       // https://docs.sentry.io/platforms/flutter/enriching-events/identify-user/
       // Set to auto to let the server decide the IP
-      Sentry.configureScope((scope) => scope.user = SentryUser(email: action.me.email, id: action.me.id, ipAddress: '{{auto}}'));
+      Sentry.configureScope((scope) => scope.user = SentryUser(
+          email: action.me.email, id: action.me.id, ipAddress: '{{auto}}'));
     }
     if (action is LogoutAction) {
       Sentry.configureScope((scope) => scope.user = null);
@@ -38,15 +37,20 @@ class SentrySdkMiddleware extends MiddlewareClass<AppState> {
   }
 
   Future<void> _enableSentrySdk() async {
-    await SentryFlutter.init(
-      (options) {
-        if (kReleaseMode) {
-          options.dsn = 'https://319565557671403383e51a7e3fae4529@o1.ingest.sentry.io/5645511';
-        } else {
-          options.dsn = 'https://b647bf95c35249fe967c91feae3f72d7@o447951.ingest.sentry.io/5555613';
-        }
+    await SentryFlutter.init((options) {
+      if (kReleaseMode) {
+        options.dsn =
+            'https://319565557671403383e51a7e3fae4529@o1.ingest.sentry.io/5645511';
+      } else {
+        options.dsn =
+            'https://b647bf95c35249fe967c91feae3f72d7@o447951.ingest.sentry.io/5555613';
       }
-    );
+
+      // This marks every stack trace frame which is not `sentry_mobile`
+      // as a not in app frame.
+      options.addInAppInclude('sentry_mobile');
+      options.considerInAppFramesByDefault = false;
+    });
   }
 
   void _disableSentrySdk() {
