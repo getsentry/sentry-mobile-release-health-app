30f6175d1485d252ec18c3129cc32267a81c3001
Denis Andrašec <denis@Deniss-MBP.home>
2021-11-10 15:40:45 +0100

    add comments

--- lib/mixin/sentry_state_transaction_mixin.dart
+++ lib/mixin/sentry_state_transaction_mixin.dart
@@ -1,10 +1,18 @@
 library after_layout;
 
+import 'dart:ui';
+
 import 'package:flutter/widgets.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 mixin SentryStateTransactionMixin<T extends StatefulWidget> on State<T> {
   ISentrySpan? transaction;
+
+  /// Per default this method returns `runtimeType.toString()`. Override to
+  /// provide a custom transaction name.
+  String transactionName() {
+    return runtimeType.toString();
+  }
 
   @override
   void initState() {
@@ -14,15 +22,12 @@ mixin SentryStateTransactionMixin<T extends StatefulWidget> on State<T> {
       'ui.load',
       bindToScope: true,
     );
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
+    // When Flutter executes the initState() method, it is in the middle of a
+    // frame rendering process. Once the frame is rendered (after build),
+    // the callback executes.
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
       transaction?.status ??= SpanStatus.ok();
       transaction?.finish();
     });
-  }
-
-  /// Per default this method returns `runtimeType.toString()`. Override to
-  /// provide a custom transaction name.
-  String transactionName() {
-    return runtimeType.toString();
   }
 }
