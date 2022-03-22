438c482fdabdcddc448b616d89213fd33ff76bde
Denis Andrašec <denis@Deniss-MacBook-Pro.local>
2022-03-22 09:17:25 +0100

    Use SentryAssetBundle

--- lib/main.dart
+++ lib/main.dart
@@ -94,78 +94,81 @@ class SentryMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-    return MaterialApp(
-      title: 'Sentry',
-      theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: SentryColors.primarySwatch,
-          dividerColor: SentryColors.lavenderGray,
 
-          // This makes the visual density adapt to the platform that you run
-          // the app on. For desktop platforms, the controls will be smaller and
-          // closer together (more dense) than on mobile platforms.
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          textTheme: GoogleFonts.rubikTextTheme(
-            TextTheme(
-                headline1: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 26,
-                  color: SentryColors.revolver,
-                ),
-                headline2: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: SentryColors.revolver,
-                ),
-                headline3: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18,
-                  color: SentryColors.revolver,
-                ),
-                headline4: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
-                  color: SentryColors.revolver,
-                ),
-                headline5: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 26,
-                  color: Colors.white,
-                ),
-                subtitle1: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14,
-                  color: SentryColors.mamba,
-                ),
-                caption: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 12,
-                  color: SentryColors.mamba,
-                )),
-          )),
-      home: StoreConnector<AppState, AppState>(
-        converter: (store) => store.state,
-        builder: (_, state) {
-          if (state.globalState.hydrated) {
-            if (state.globalState.authToken == null) {
-              return OnboardingScreen();
-            } else {
-              return MainScreen();
-            }
-          } else {
-            return SplashScreen();
-          }
-        },
-      ),
-      navigatorObservers: [SentryNavigatorObserver()],
-    );
+    return DefaultAssetBundle(
+        bundle: SentryAssetBundle(),
+        child: MaterialApp(
+          title: 'Sentry',
+          theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: SentryColors.primarySwatch,
+              dividerColor: SentryColors.lavenderGray,
+
+              // This makes the visual density adapt to the platform that you run
+              // the app on. For desktop platforms, the controls will be smaller and
+              // closer together (more dense) than on mobile platforms.
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+              textTheme: GoogleFonts.rubikTextTheme(
+                TextTheme(
+                    headline1: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 26,
+                      color: SentryColors.revolver,
+                    ),
+                    headline2: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: SentryColors.revolver,
+                    ),
+                    headline3: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18,
+                      color: SentryColors.revolver,
+                    ),
+                    headline4: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: SentryColors.revolver,
+                    ),
+                    headline5: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                    subtitle1: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14,
+                      color: SentryColors.mamba,
+                    ),
+                    caption: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 12,
+                      color: SentryColors.mamba,
+                    )),
+              )),
+          home: StoreConnector<AppState, AppState>(
+            converter: (store) => store.state,
+            builder: (_, state) {
+              if (state.globalState.hydrated) {
+                if (state.globalState.authToken == null) {
+                  return OnboardingScreen();
+                } else {
+                  return MainScreen();
+                }
+              } else {
+                return SplashScreen();
+              }
+            },
+          ),
+          navigatorObservers: [SentryNavigatorObserver()],
+        ));
   }
 }
