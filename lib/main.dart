a6590cc10721cf07f64da1345b1032296d79f8de
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2022-04-12 09:35:50 +0200

    Bump Flutter SDK to 6.5.0-beta.1 and enable other features (#236)
    Co-authored-by: Denis Andrašec <denis@Deniss-MBP.home>

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class SentryMobile extends StatelessWidget {
         SystemUiOverlayStyle(statusBarColor: Colors.transparent));
 
     return DefaultAssetBundle(
-        bundle: SentryAssetBundle(),
+        bundle: SentryAssetBundle(enableStructuredDataTracing: true),
         child: MaterialApp(
           title: 'Sentry',
           theme: ThemeData(
